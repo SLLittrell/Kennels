854d6dfebf5ecf8ac47c233596fd441a96e3993d
stacey.littrell@gmail.com <slittrell@WINDOWS-HNR6UL2.localdomain>
2021-02-26 16:14:12 -0600

    customer and employee coponents render

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,10 +2,12 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
 import { LocationCard } from "./locations/Location";
-import {EmployeeCard } from "./employees/Employee";
-import { CustomerCard } from "./custumers/Customer";
+import { EmployeeCard } from "./employees/Employee";
+import { CustomerProvider } from "./custumers/CustomerProvider";
+import { CustomerList } from "./custumers/CustomerList";
 import { AnimalProvider } from "./animals/AnimalProvider"
 import { AnimalList } from "./animals/AnimalList"
+import { EmployeeProvider } from "./employees/EmployeeProvider";
 //rerouting to components when nav is clicked
 //Route matched with Link on NavBar
 export const ApplicationViews = () => {
@@ -29,14 +31,18 @@ export const ApplicationViews = () => {
             </Route>
 
             {/* Render the animal list when http://localhost:3000/customers */}
-            <Route path="/customers">
-                <CustomerCard />
-            </Route>
+            <CustomerProvider>
+                <Route path="/customers">
+                    <CustomerList />
+                </Route>
+            </CustomerProvider>
 
             {/* Render the animal list when http://localhost:3000/employees */}
-            <Route path="/employees">
-                <EmployeeCard />
-            </Route>
+            <EmployeeProvider>
+                <Route path="/employees">
+                    <EmployeeCard />
+                </Route>
+            </EmployeeProvider>
         </>
     )
 }
--- src/components/custumers/Customer.js
+++ src/components/custumers/Customer.js
@@ -1,9 +1,9 @@
 import React from "react"
 import "./Customers.css"
 
-export const CustomerCard = () => (
+export const CustomerCard = ({customer}) => (
     <section className="customer">
-        <h3 className="customer__name">Hannah Hall</h3>
-        <div className="address__breed">Address: 1000 Infinity Way</div>
+        <h3 className="customer__name">{customer.name}</h3>
+        <div className="customer__address">{customer.address}</div>
     </section>
 )
