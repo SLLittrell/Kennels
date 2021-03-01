847137f9c61977314d01cb1c00db2c5d2a246e77
stacey.littrell@gmail.com <slittrell@WINDOWS-HNR6UL2.localdomain>
2021-03-01 11:48:38 -0600

    refactored animals to include Customer and Location data

--- src/components/animals/AnimalCard.js
+++ src/components/animals/AnimalCard.js
@@ -1,9 +1,11 @@
 import React from "react"
 import "./Animal.css"
 
-export const AnimalCard = ({ animal }) => (
+export const AnimalCard = ({ animal, customer, location }) => (
     <section className="animal">
         <h3 className="animal__name">{animal.name}</h3>
-        <address className="location__address">{animal.location.name}</address>
+        <p className="animal__breed">{animal.breed}</p>
+        <address className="location__address">Location:{location.name}</address>
+        <p className="animal__owner">Customer:{customer.name}</p>
     </section>
 )
--- src/components/animals/AnimalList.js
+++ src/components/animals/AnimalList.js
@@ -1,21 +1,34 @@
 import React, { useContext, useEffect } from "react"
 import { AnimalContext } from "./AnimalProvider"
+import { CustomerContext } from "../custumers/CustomerProvider";
+import { LocationContext } from "../locations/LocationProvider";
 import { AnimalCard } from "./AnimalCard"
 import "./Animal.css"
 
 export const AnimalList = () => {
     const {animals, getAnimals} = useContext(AnimalContext)
+    const {customers, getCustomers} = useContext(CustomerContext)
+    const {locations, getLocations} = useContext(LocationContext)
 
     useEffect(() => {
         console.log("AnimalList: useEffect - getAnimals")
-        getAnimals()
+        getLocations()
+            .then(getCustomers)
+            .then(getAnimals)
     }, []
     )
     return (
     <div className="animals">
-      {console.log("AnimalList: Render", animals)}
       {
-        animals.map(animal => <AnimalCard key={animal.id} animal={animal} />)
+        animals.map(animal => {
+          const owner = customers.find(cust => cust.id === animal.customerId)
+          const location = locations.find(local => local.id === animal.locationId)
+
+          return <AnimalCard key={animal.id}
+                  location = {location}
+                  customer={owner}
+                  animal = {animal} />
+        })
       }
     </div> 
     )
